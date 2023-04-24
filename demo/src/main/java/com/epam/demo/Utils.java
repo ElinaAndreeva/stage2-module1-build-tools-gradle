d8092ed4d0d0113fa1883a8e5e77941d6915427b
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-24 11:12:02 +0400

    Completed the task

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,6 +2,8 @@ package com.epam.demo;
 
 import java.util.List;
 
+import com.epam.utils.StringUtils;
+
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         return args.stream().allMatch(StringUtils::isPositiveNumber);
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         return NumberUtils.isParsable(str) && Double.parseDouble(str) > 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("12"));
     }
 }
